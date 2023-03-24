bb47f67d51f486ee86bc069eaa61e5ad04cc7db6
cataafra <afr.catalin@gmail.com>
2023-03-24 20:43:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// let navItems = [
+//   "World",
+//   "U.S.",
+//   "Politics",
+//   "N.Y.",
+//   "Business",
+//   "Opinion",
+//   "Science",
+//   "Health",
+//   "Sports",
+//   "Arts",
+//   "Books",
+//   "Style",
+//   "Food",
+//   "Travel",
+//   "Magazine",
+//   "Real Estate",
+//   "Cooking",
+//   "The Athletic",
+//   "Wirecutter",
+//   "Games",
+// ];
+
+// var navBar = document.getElementsByClassName("nav-bar")[0];
+
+// navItems.forEach((a) => {
+//   let navBarElem = document.createElement("span");
+//   navBarElem.classList.add("nav-item");
+//   navBarElem.innerText = a;
+//   navBar.appendChild(navBarElem);
+// });
+
+// const news = [
+//   "Home Page",
+//   "World",
+//   "Coronavirus",
+//   "U.S.",
+//   "Politics",
+//   "New York",
+//   "Business",
+//   "Tech",
+//   "Science",
+//   "Sports",
+//   "Wildfire Tracker",
+//   "Obituaries",
+//   "Today's Paper",
+//   "Corrections",
+//   "Trending",
+// ];
+
+// var footerCol = document.getElementsByClassName("footer-col")[0];
+// console.log(footerCol);
+
+// news.forEach((str) => {
+//   let footerElem = document.createElement("a");
+//   footerElem.classList.add("footer-item");
+//   footerCol.setAttribute("href", "#");
+//   footerElem.innerText = str;
+//   footerCol.appendChild(footerElem);
+// });
